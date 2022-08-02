47565f3823a642ba1ebeaec463eb1e19dcd61bad
MartyJRE <33270024+MartyJRE@users.noreply.github.com>
2022-08-02 23:48:18 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd &&
-  [ -d "./configs" ] && 
   (
-    (
-      cd configs &&
-        git pull && 
-        cd .. &&
-        echo "Configs folder existed - pulled from repository"
-    ) ||
-    (
-        git clone git@github.com:MartyJRE/configs.git &&
-          echo "Cloned configs to home directory"
-    )
+    [ -d "./configs" ] && 
+      (
+        cd configs &&
+          git pull && 
+          cd .. &&
+          echo "Configs folder existed - pulled from repository"
+      ) ||
+      (
+          git clone git@github.com:MartyJRE/configs.git &&
+            echo "Cloned configs to home directory"
+      )
   ) &&
   cp -R ./configs/.config/* ./.config &&
   sudo cp ./configs/pacman/pacman.conf /etc/pacman.conf &&
