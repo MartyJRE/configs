cca72b14715f8957666c62c727e64051db834df7
Martin Pražák <martin.prazak@lutherx.com>
2022-08-02 23:39:05 +0200

    install script

---
+++ install/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd &&
+  [ -d "./configs" ] && 
+  (
+    (
+      cd configs &&
+        git pull && 
+        cd .. &&
+        echo "Configs folder existed - pulled from repository"
+    ) ||
+    (
+        git clone git@github.com:MartyJRE/configs.git &&
+          echo "Cloned configs to home directory"
+    )
+  ) &&
+  cp -R ./configs/.config/* ./.config &&
+  cp ./configs/pacman/pacman.conf /etc/pacman.conf &&
+  rsync -am ./configs ./ &&
+  echo "Copied all configurations to their desired locations"
